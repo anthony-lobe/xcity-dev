d8c1b626853631a374e6002a96d9899486755bae
Antho <emiliekassa-mapsi@MacBook-Pro-danthony.local>
2020-11-03 00:46:50 +0100

    ++++ correcting some errors

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 
     if (isset($_POST['submit'])) {
-        $email = $_POST('email');
-        $pass = $_POST('pass');
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
 
         echo $email;
     }
