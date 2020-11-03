dd34c9626f637267b9c7ad55c2dc8fd627f00b43
Antho <emiliekassa-mapsi@MacBook-Pro-danthony.local>
2020-11-03 13:49:37 +0100

    sending files

--- login.php
+++ login.php
@@ -2,11 +2,28 @@
 
     if (isset($_POST['submit'])) 
     {
-        echo "ok";  
-        //$email = $_POST['email'];
-        ///$pass = $_POST['pass'];
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+        
+        $db = new PDO('mysql:host=localhost;dbname=xcitydb', 'root', '');
 
-        //echo $email;
+        $sql = "SELECT * FROM users where email = '$email' ";
+        $result = $db->prepare($sql);
+        $result->execute();
+
+        if($result->rowCount() > 0)
+        {
+
+        }
+        else 
+        {
+            $pass = password_hash($pass, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (email, password) VALUES ('$email', '$pass')";
+            $req = $db->prepare($sql);
+            $req->execute();
+            echo "enregistrement terminé";
+
+        }
     }
 
 ?>
