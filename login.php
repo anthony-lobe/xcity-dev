5ad52af4235eeb15b046cf94849ba8fda23fb780
Antho <emiliekassa-mapsi@MacBook-Pro-danthony.local>
2020-11-03 12:36:56 +0100

    changing the files html in php

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
 
-    if (isset($_POST['submit'])) {
-        $email = $_POST['email'];
-        $pass = $_POST['pass'];
+    if (isset($_POST['submit'])) 
+    {
+        echo "ok";  
+        //$email = $_POST['email'];
+        ///$pass = $_POST['pass'];
 
-        echo $email;
+        //echo $email;
     }
 
 ?>
